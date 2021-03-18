eec8cd50451ef3078dd6bcf813f2035cfef1a63c
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-18 22:28:29 +0400

    catching and logging the error is important

--- Program.cs
+++ Program.cs
@@ -33,8 +33,16 @@ namespace SemaphoreUsage
 
         private static async Task CallGoogle()
         {
-            var response = await _client.GetAsync("https://google.com");
-            Console.WriteLine(response.StatusCode);
+            try
+            {
+                var response = await _client.GetAsync("https://google.com");
+                Console.WriteLine(response.StatusCode);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
