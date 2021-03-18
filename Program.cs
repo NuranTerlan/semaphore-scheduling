275ac5a1ccf108ce5a67088bba8832850e7731c0
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-18 21:59:02 +0400

    semaphore usage is added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreUsage
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var gate = new SemaphoreSlim(2);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Start");
+                await gate.WaitAsync();
+                Console.WriteLine("Do Some Work");
+                Console.WriteLine("Finish");
+            }
+        }
+    }
+}
